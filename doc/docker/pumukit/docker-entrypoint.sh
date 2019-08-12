08bc2c876ded86b50ab58cce08d49f3504c3308f
Anxo <aduran@teltek.es>
2019-08-12 11:21:27 +0200

    Adds an environment var as a flag for user autocreation

--- doc/docker/pumukit/docker-entrypoint.sh
+++ doc/docker/pumukit/docker-entrypoint.sh
@@ -16,10 +16,13 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
     if [ "$APP_ENV" != 'prod' ]; then
         composer install --prefer-dist --no-progress --no-suggest --no-interaction
         bin/console doctrine:mongodb:schema:create
-	bin/console pumukit:init:repo all --force
+		bin/console pumukit:init:repo all --force
+    	if [ "$AUTOCREATE_PUMUKIT_USER" == 'true']; then
+			set +e
+			php bin/console fos:user:create $PUMUKIT_USER $PUMUKIT_MAIL $PUMUKIT_PASS || true
+			set -e
+		fi
     fi
 fi
-set +e
-php bin/console fos:user:create $PUMUKIT_USER $PUMUKIT_MAIL $PUMUKIT_PASS || true
-set -e
+
 exec docker-php-entrypoint "$@"
