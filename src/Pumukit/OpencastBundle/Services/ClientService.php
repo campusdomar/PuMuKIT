094797fed4321e0cf35a2da3b3602a151b7bd181
Pablo Nieto <pnieto@teltek.es>
2019-11-14 12:41:30 +0100

    fallback if mediackage not exists in archive

--- src/Pumukit/OpencastBundle/Services/ClientService.php
+++ src/Pumukit/OpencastBundle/Services/ClientService.php
@@ -255,7 +255,13 @@ class ClientService
         }
 
         if ($version >= '1.4.0' && $version < '1.7.0') {
-            return $this->getMediaPackageFromArchive($id);
+            $mediapackage = $this->getMediaPackageFromArchive($id);
+            
+            if ( $mediapackage == false ) {
+                $mediapackage = $this->getMediaPackageFromWorkflow($id);
+            }
+            
+            return $mediapackage;
         }
 
         if (0 == strpos($version, '1.2')) {
